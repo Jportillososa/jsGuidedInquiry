5140c074111e3177c4e55fed832ce58f88f869d0
Jose Portillo <jportillososa@MacBook-Pro.attlocal.net>
2022-04-15 12:28:24 -0400

    Completion of Guided Inquiry

--- 4-Hard/hard.js
+++ 4-Hard/hard.js
@@ -2,33 +2,27 @@
 // called multiply() to multiply 2 numbers, and one called divide() to divide two numbers.
 
 
-// function add(number) {
-//     return number * number;
+//Addition
+function add(number1, number2) {
+    return number1 + number2;
 
-// }
+}
+
+//Subtraction
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+//Multiplication
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+//Division
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+console.log(add(99, 40), subtract(10, 7), multiply(9, 9), divide(50, 5));
+
 // console.log(number);
-
-// function subtract(number) {
-//     return number * number;
-// }
-
-// function multiply(number) {
-//     return number * number;
-// }
-
-// function divide(number) {
-//     return number * number;
-// }
-
-// console.log(`${number1} ${operator} ${number2} = ${result}`);
-
-
-const x = 11;
-const y = 9;
-
-let add = x + y;
-let subtract = x - y;
-let multiply = x * y;
-let divide = x / y;
-
-console.log(add);
